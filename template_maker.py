dc0cf0a530f1ff345d65a17990b8c98719660822
Mark Harrison <mark@mivok.net>
2013-07-14 23:25:07 -0400

    Add some more tests, and make camel_to_underscore pass them

--- template_maker.py
+++ template_maker.py
@@ -14,6 +14,7 @@ import re
 import sys
 import argparse
 
+
 def camel_to_underscore(s):
     """
     Converts CamelCase to underscore_case
@@ -26,14 +27,22 @@ def camel_to_underscore(s):
     'rhosts_rsa_authentication'
     >>> camel_to_underscore("X11Forwarding")
     'x11_forwarding'
-    >>> camel_to_underscore("TestExampleRSA")
-    'test_example_rsa'
+    >>> camel_to_underscore("TestExampleABC")
+    'test_example_abc'
+    >>> camel_to_underscore("TestExampleA")
+    'test_example_a'
+    >>> camel_to_underscore("ATestExample")
+    'a_test_example'
     """
     new_s = []
     for i, c in enumerate(s):
         next_is_upper = i < len(s) - 1 and s[i+1].isupper()
-        if c.isupper() and not next_is_upper and i != 0:
-            new_s.append('_')
+        prev_is_upper = i > 0 and s[i-1].isupper()
+        if i > 0:  # Never include an underscore at the beginning
+            if c.isupper() and not next_is_upper and i < len(s) - 1:
+                new_s.append('_')
+            elif c.isupper() and not prev_is_upper:
+                new_s.append('_')
         new_s.append(c.lower())
     return ''.join(new_s)
 
